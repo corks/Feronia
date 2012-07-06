f2272376a2b94d3e24954d850301e316292ff715
Michael Rodriguez <adeo2232@gmail.com>
2012-07-06 18:20:17 -0400

    Begin to work on the Sphinx documentation generation configuration.

---
+++ doc/src/conf.py
@@ -0,0 +1,8 @@
+"""
+Sphinx configuration file. This file serves to provide the information needed to
+generate HTML and PDF based documentation for the Feronia project. Be advised,
+generators other than HTML and PDF are not supported by the Feronia team.
+
+For a list of configuration options not specified here, please see
+http://is.gd/0n9l6I.
+"""
